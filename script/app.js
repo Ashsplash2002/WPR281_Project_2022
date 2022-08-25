e1877899b18d15cf4985ad3360af903d8bc89526
Ruhan Potgieter <ruhan@sietechnn.co.za>
2022-08-25 11:55:07 +0200

    just did a popup form when pressing the add button (still trying to figure out how to make it display ))
    basically die popup form is gedesign sal nog op hom

--- script/app.js
+++ script/app.js
@@ -1,3 +1,40 @@
-// function that displays data from the database and displays it in the table on the page with selecting the number of entries to display
 
- 
+//simple array of words to list the common areas of problem
+var select = document.getElementById("selectName");
+var option = ["john", "jane", "joe", "jill"];
+
+for (var i = 0; i < option; i++) {
+    var opt = option[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+    
+}
+// simple array of names to display in the select box
+var select1 = document.getElementById("selectDepartment");
+var option1 = ["john", "jane", "joe", "jill"];
+
+for (var j = 0; j < option; j++) {
+    var opt1 = option[j];
+    var el1 = document.createElement("option");
+    el1.textContent = opt1;
+    el1.value = opt1;
+    select.appendChild(el1);
+    
+}
+
+document.querySelector("#show-popup").addEventListener("click", function () 
+{
+document.querySelector(".popup").classList.add("active");
+});
+
+document.querySelector(".popup .close-btn").addEventListener("click", function () 
+{
+document.querySelector(".popup").classList.add("active");
+});
+
+
+
+
+
